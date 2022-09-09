9e6a1d51fdf390ba52ac33cd439b7bdc32d65826
pi0723 <coco_mel.0723@icloud.com>
2022-09-09 15:00:27 +0900

    マイグレーション実行／バリデーション作成

--- db/migrate/20220909054009_devise_create_users.rb
+++ db/migrate/20220909054009_devise_create_users.rb
@@ -7,11 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false
-      t.date   :birthday            null: false
-      t.string :last_name           null: false
-      t.strong :first_name          null: false
-      t.string :read_last           null: false
-      t.string :read_first          null: false
+      t.date   :birthday,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :read_last,          null: false
+      t.string :read_first,         null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
