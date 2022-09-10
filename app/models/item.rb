9af6b2561255dfea716fd1fad42508e493beea4d
pi0723 <coco_mel.0723@icloud.com>
2022-09-10 16:25:45 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :postage
   belongs_to :area
   belongs_to :delivery_time
-  
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality:      { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality:     { other_than: 1, message: "can't be blank" }
+  validates :postage_id, numericality:       { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality:          { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
