4ecef47ce891c0fddc8a044aa1afdcb6f7723822
pi0723 <coco_mel.0723@icloud.com>
2022-09-10 16:35:55 +0900

    画像投稿機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -7,13 +8,14 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :delivery_time
 
-  validates :name, presence: true
+  validates :name,        presence: true
   validates :explanation, presence: true
-  validates :price, presence: true
-  validates :category_id, numericality:      { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality:     { other_than: 1, message: "can't be blank" }
-  validates :postage_id, numericality:       { other_than: 1, message: "can't be blank" }
-  validates :area_id, numericality:          { other_than: 1, message: "can't be blank" }
+  validates :price,       presence: true
+  validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,     numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_id,       numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id,          numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image,       presence: true 
 
 end
