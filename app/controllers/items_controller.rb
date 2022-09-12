6ae017103da30ddd618bb60b001f04de75b9d259
pi0723 <coco_mel.0723@icloud.com>
2022-09-12 15:50:31 +0900

    ビューファイル編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,14 +24,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.new(item_params)
-    if @item.save
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
