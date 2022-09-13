ed1949bd3be4e692cbbce2c6f8c57cc376510cf6
pi0723 <coco_mel.0723@icloud.com>
2022-09-13 15:53:32 +0900

    ビューファイル編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,11 @@ class OrdersController < ApplicationController
 
   def index
     @order_destination = OrderDestination.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    binfing.pry
+    @item = Item.find(params[:item_id])
     @order_destination = OrderDestination.new(order_params)
     if @order_destination.valid?
       @order_destination.save
@@ -18,8 +19,8 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:donation_address)
-          .permit( :post_code, :city, :address, :building, :phone, :area_id, :order_id)
+    params.require(:order_destination)
+          .permit(:post_code, :city, :address, :building, :phone, :area_id, :order_id)
           .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
